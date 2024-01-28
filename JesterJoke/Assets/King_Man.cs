bfa50e3be3c1911d4dd0d1d8a778867c5a4d0e68
Ben <56559770+Bendude5@users.noreply.github.com>
2024-01-28 12:00:14 +0000

    Progress5

--- JesterJoke/Assets/King_Man.cs
+++ JesterJoke/Assets/King_Man.cs
@@ -49,4 +49,9 @@ public class King_Man : MonoBehaviour
     {
         SceneManager.LoadScene("Game_Over_Scene");
     }
+
+    public void loadGameWin()
+    {
+        SceneManager.LoadScene("Game_Win");
+    }
 }
